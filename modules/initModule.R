ecd42eee31f4cf977814c889c6d1c3c40cee723f
Olga Muraeva <muraeva.olya@gmail.com>
2022-08-26 11:51:58 +0400

    QR-1: draft UI

--- modules/initModule.R
+++ modules/initModule.R
@@ -1,13 +1,24 @@
 initUI <- function(id) {
     ns <- NS(id)
     tagList(
-        h5("You can take an R/Shiny skill assessment within this app."),
+        h3("You can take an R/Shiny skill assessment within this app"),
+        selectInput(
+            ns("topic"),
+            "Choose a topic",
+            choices = c(
+                "R" = "r",
+                "R Shiny" = "shiny",
+                "R packages" = "packages",
+                "CI/CD" = "ci"
+            ),
+            selected = "r",
+            multiple = TRUE
+        ),
         actionButton(ns("start"), "Start test")
     )
 }
 
 initServer <- function(id) {
     moduleServer(id, function(input, output, session) {
-
     })
 }
